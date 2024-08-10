156136d46b23dca0435ebce8f67f716a277babc8
jash <jash@live.in>
2024-08-10 16:01:33 -0400

    updated url
    Signed-off-by: jash <jash@live.in>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:  ["https://superhack-2024.vercel.app/","http://localhost:3000"] , // Allow requests from localhost:3000
+    origin:  ["https://superhack-2024.vercel.app","http://localhost:3000"] , // Allow requests from localhost:3000
     methods: 'POST,OPTIONS',
     credentials: true, // Allow credentials such as cookies
   });
